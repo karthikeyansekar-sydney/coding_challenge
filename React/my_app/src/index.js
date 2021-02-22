21d0ac64fc77a7e8d93ec95b2b53c7c4259b5012
karthikeyansekar-sydney <karthikeyansep27@gmail.com>
2021-02-22 21:25:21 +1100

    React with if else statement

---
+++ React/my_app/src/index.js
@@ -0,0 +1,44 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
+//
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+//
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+function coinToss() {
+  // This function will randomly return either 'heads' or 'tails'.
+  return Math.random() < 0.5 ? 'heads' : 'tails';
+}
+
+const pics = {
+  kitty: 'https://content.codecademy.com/courses/React/react_photo-kitty.jpg',
+  doggy: 'https://content.codecademy.com/courses/React/react_photo-puppy.jpeg'
+};
+let img;
+
+// if/else statement begins here:
+if (coinToss() === "heads")
+{
+img = (<img src={pics.kitty}/>);
+}
+else
+{
+img = (<img src={pics.doggy}/>);
+}
+
+ReactDOM.render(img, document.getElementById('root'));
