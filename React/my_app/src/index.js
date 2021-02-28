a7d865fe07bc470741b7fede6fa915b126810c4e
karthikeyansekar-sydney <karthikeyansep27@gmail.com>
2021-02-28 21:10:02 +1100

    Introducing class component

--- React/my_app/src/index.js
+++ React/my_app/src/index.js
@@ -55,8 +55,30 @@ img = (<img src={pics.doggy}/>);
 }
 */
 
+
+class QuoteMaker extends React.Component {
+  render() {
+    return (
+      <blockquote>
+        <p>
+          The world is full of objects, more or less interesting; I do not wish to add any more.
+        </p>
+        <cite>
+          <a target="_blank"
+            href="https://en.wikipedia.org/wiki/Douglas_Huebler">
+            Douglas Huebler
+          </a>
+        </cite>
+      </blockquote>
+    );
+  }
+};
+
+
 const img = <img src={ coinToss() === 'heads' ? pics.kitty : pics.doggy} />;
 
-ReactDOM.render(peopleLis, document.getElementById('root'));
+//ReactDOM.render(peopleLis, document.getElementById('root'));
 
 //ReactDOM.render(img, document.getElementById('root'));
+
+ReactDOM.render(<QuoteMaker/>, document.getElementById('root'));
