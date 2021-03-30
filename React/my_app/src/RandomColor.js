4b3c8987b41f686d7e8cc5607cb1f542b9dd8b75
karthikeyansekar-sydney <karthikeyansep27@gmail.com>
2021-03-31 00:30:29 +1100

    random color project - 1

---
+++ React/my_app/src/RandomColor.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Random extends React.Component {
+
+   constructor(props) {
+    super(props);
+    this.state = { color: [2, 100, 200] };
+  }
+}
